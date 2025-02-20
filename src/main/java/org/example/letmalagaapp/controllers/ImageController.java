47964ff4b661b375594808988abf14e757da49d3
Hugo Romero <hugoromerosmr@gmail.com>
2025-02-20 18:32:41 +0100

    nujjk

--- src/main/java/org/example/letmalagaapp/controllers/ImageController.java
+++ src/main/java/org/example/letmalagaapp/controllers/ImageController.java
@@ -1,43 +1,34 @@
 package org.example.letmalagaapp.controllers;
 
-        import org.springframework.http.HttpHeaders;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-        import java.io.IOException;
-        import java.nio.file.Files;
-        import java.nio.file.Path;
-        import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-        /**
-         * Controlador para manejar las solicitudes relacionadas con las imágenes.
-         */
-        @RestController
-        @RequestMapping("/images")
-        @CrossOrigin(origins = "http://localhost:3000")
-        public class ImageController {
+@RestController
+@RequestMapping("/images")
+@CrossOrigin(origins = "http://localhost:3000")
+public class ImageController {
 
-            private static final String UPLOAD_DIR = "src/main/resources/static/uploads/";
+    private static final String UPLOAD_DIR = "src/main/resources/static/uploads/";
 
-            /**
-             * Maneja la solicitud para obtener una imagen por su nombre de archivo.
-             *
-             * @param filename el nombre del archivo de la imagen
-             * @return la imagen en formato de bytes si se encuentra, de lo contrario, una respuesta de error 404
-             */
-            @GetMapping("/{filename}")
-            public ResponseEntity<byte[]> getImage(@PathVariable String filename) {
-                try {
-                    Path imagePath = Paths.get(UPLOAD_DIR, filename);
-                    byte[] imageBytes = Files.readAllBytes(imagePath);
+    @GetMapping("/{filename}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String filename) {
+        try {
+            Path imagePath = Paths.get(UPLOAD_DIR, filename);
+            byte[] imageBytes = Files.readAllBytes(imagePath);
 
-                    HttpHeaders headers = new HttpHeaders();
-                    headers.add("Content-Type", Files.probeContentType(imagePath));
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", Files.probeContentType(imagePath));
 
-                    return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
-                } catch (IOException e) {
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-                }
-            }
+            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
+    }
+}
